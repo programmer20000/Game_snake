52515c3d18878093aef57e0d07ea47c30a3317cb
progrmmer <84572292+programmer20000@users.noreply.github.com>
2022-04-22 10:44:24 +0300

    was changes project

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ head_size = 50
 font = pygame.font.SysFont("Arial", 50)
 position_head_x = 0
 position_head_y = 0
+length = 1
 snake = [(position_head_x,position_head_y)]
 score = 0
 FPS = 60
 
 screen = pygame.display.set_mode([WIDTH_WINDOW, HEIGHT_WINDOW])
 pygame.display.set_caption(TITLE_WINDOW)
+
+pygame.mixer.music.load("music/Snake Game - Theme Song.mp3")
+pygame.mixer.music.play(-1)
 
 
 class Player(pygame.sprite.Sprite):
@@ -29,26 +33,18 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(color=color)
         self.rect = self.image.get_rect()
 
+        self.speed = 10
+
     def position(self, position_head_x, position_head_y):
         self.rect.x = position_head_x
         self.rect.y = position_head_y
 
     def movement_player(self, x_direction, y_direction):
-        self.rect.x += x_direction
-        self.rect.y += y_direction
-
-    def update(self):
-        self.rect.x += 1
+        self.rect.x += x_direction * self.speed
+        self.rect.y += y_direction * self.speed
 
 
-
-class Food(pygame.sprite.Sprite):
-    def __init__(self, width, height, color):
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color=color)
-        self.rect = self.image.get_rect()
-
+class Food(Player):
     def random_position(self):
         self.rect.x = random.randrange(WIDTH_WINDOW - 100)
         self.rect.y = random.randrange(HEIGHT_WINDOW - 100)
@@ -66,8 +62,11 @@ player = Player(width=head_size, height=head_size, color=SNAKE_COLOR)
 player.position(position_head_x=position_head_x, position_head_y=position_head_y)
 all_sprites_group.add(player)
 
-def draw_snake_tail():
-    [pygame.draw.rect(screen, SNAKE_COLOR, (block_x, block_y, head_size, head_size)) for block_x,block_y in snake]
+
+def snake_tails():
+    [pygame.draw.rect(screen,SNAKE_COLOR,(i,j,head_size,head_size)) for i,j in snake]
+    snake.append((position_head_x,position_head_y))
+    pygame.display.flip()
 
 Close_Window = False
 clock = pygame.time.Clock()
@@ -76,6 +75,10 @@ while not Close_Window:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Close_Window = True
+
+    if position_head_x > WIDTH_WINDOW:
+        Close_Window = True
+
     screen.fill(BACKGROUND_COLOR_WINDOW)
     all_sprites_group.draw(screen)
     all_sprites_group.update()
@@ -106,12 +109,12 @@ while not Close_Window:
 
     sprites_collision = pygame.sprite.spritecollide(player, sprites_group, False)
     if food in sprites_collision:
+        snake_tails()
         score += 1
+        length +=1
         food.random_position()
-        draw_snake_tail()
-        pygame.display.flip()
 
     show_score = font.render("Score:" + str(score), True, SCORE_COLOR)
-    screen.blit(show_score,[220,0])
+    screen.blit(show_score, [220, 0])
     pygame.display.update()
     clock.tick(FPS)
